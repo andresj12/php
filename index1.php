3afa70de1ffa11bca978ad333703bac621867979
Janus <janusdeb@localhost.localdomain>
2019-06-19 12:41:16 -0500

    Miercoles

--- index1.php
+++ index1.php
@@ -1,30 +1,17 @@
 <?php
-
-// Path de uploaded files
-$target_path = dirname(__FILE__).'/uploads/';
-  
-if (isset($_FILES['image']['name'])) {
-    // $target_path = $target_path . basename($_FILES['image']['name']);//nombre de la imagen
-    $target_path = $target_path . 'image.jpg';
- 
-    try {
-        // Throws exception si la imagen no se subio
-        if (move_uploaded_file($_FILES['image']['tmp_name'], $target_path)) {
-            
-  
-            $ch = curl_init('https://api.clarifai.com/v2/models/' . 'flores'. '/outputs'); 
+$ch = curl_init('https://api.clarifai.com/v2/models/' . 'flores'. '/outputs'); 
 
              $key = '68817275177a4699a4c70263e5b6fcfc';
             // $key = 'd6a51f0e10bb42b283e39037ac84f7c1';
              
 
-            $ar =	array("inputs" => array("data" => array("image" => array("url" => "https://reconocimiento.herokuapp.com/uploads/image.jpg"))));
+            $ar =	array("inputs" => array("data" => array("image" => array("url" => "http://192.168.1.3:8080/Mineria/uploads/image.jpg"))));
             $data=  '{
                 "inputs": [
                     {
                 "data": {
                 "image": {
-                    "url": "https://reconocimiento.herokuapp.com/uploads/image.jpg"
+                    "url": "http://192.168.1.3:8080/Mineria/uploads/image.jpg"
                             }
                         }
                     }
@@ -52,23 +39,4 @@ if (isset($_FILES['image']['name'])) {
 		//echo  json_decode($output, TRUE);
 		//var_dump($output);
 //		echo $output;
-            
-            
-        } else {
-            // make error flag true
-            echo json_encode(array('status'=>'fail', 'message'=>'could not move file'));
-
-         
-       }
- 
-  } catch (Exception $e) {
-        // Exception occurred. Make error flag true
-        echo json_encode(array('status'=>'fail', 'message'=>$e->getMessage()));
-  }
-} else {
-    // File parameter is missing
-    echo json_encode(array('status'=>'fail', 'message'=>'Not received any file'));
-}
-
-
-?>
+    ?>        
---
+++ upload.php
@@ -0,0 +1,31 @@
+<?php 
+require_once 'uploads/vendor/autoload.php';
+
+use Kunnu\Dropbox\Dropbox;
+use Kunnu\Dropbox\DropboxApp;
+
+$dropboxKey ="ijgcvjb4unw73iv";
+$dropboxSecret ="13tecf456t8jicw";
+$dropboxToken="AQ9Q1JqhF2AAAAAAAAAAK03agk0bTIsjjGyYzJ6Vn87ZhiqLNJLS5IEcBfjX5HeN";
+
+$app = new DropboxApp($dropboxKey,$dropboxSecret,$dropboxToken);
+$dropbox = new Dropbox($app);
+
+//Si la variable archivo no esta vacia
+if(!empty($_FILES)){
+    $nombre = image;
+    $tempfile = $_FILES['file']['tmp_name'];
+    $ext = explode(".",$_FILES['file']['name']);
+    $ext = end($ext);
+    $nombredropbox = "/" .$nombre . "." .$ext;
+
+   try{
+        $file = $dropbox->simpleUpload( $tempfile,$nombredropbox, ['autorename' => true]);
+        echo "Archivo subido en dropbox";
+        include './index1.php';
+   }catch(\exception $e){
+        print_r($e);
+        
+   }
+}
+?>
